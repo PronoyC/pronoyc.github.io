58072a238e5c2b6cc76c6800c0538b9f70e2eeaa
Pronoy Chaudhuri <pronoyy@gmail.com>
2018-07-22 14:15:32 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        // cache.add('/');
+        // cache.add('/index.html');
+        // cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/_scripts/main.js',
+          '/_css/main.css',
+          '/_scss/main.scss',
+          '/_layouts/default.html'
+        ]);
+      })
+  );
+});
 
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});

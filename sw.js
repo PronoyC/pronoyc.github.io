7c36e48674afc9c86779891f210e71a1fab72ada
Pronoy Chaudhuri <pronoyy@gmail.com>
2018-07-22 12:01:28 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-‘use strict’;
-importScripts(‘sw-toolbox.js’); 
-toolbox.precache([“index.html”,”css/main.css”,"_scss/main.scss"]); 
-toolbox.router.get(‘/img/*’, toolbox.cacheFirst); 
-toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+'use strict';
+
+importScripts('sw-toolbox.js');
+
+toolbox.precache([“index.html”,”css/main.css”,"_scss/main.scss", "_layouts/default.html"]); 
+
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+
+toolbox.router.get('/*', toolbox.networkFirst, {
+  networkTimeoutSeconds: 5
+});
